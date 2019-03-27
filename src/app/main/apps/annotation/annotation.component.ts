59b958cf81b1f26cbac2c812a8eac23392d3e11b
tmushayahama <tmushayahama@gmail.com>
2019-03-27 11:06:26 -0700

    added the other forms

--- src/app/main/apps/annotation/annotation.component.ts
+++ src/app/main/apps/annotation/annotation.component.ts
@@ -27,9 +27,37 @@ export class AnnotationComponent implements OnInit {
   createAnnotationForm() {
     return new FormGroup({
       chrom: new FormControl(18),
+      chromList: new FormControl(),
+      geneProduct: new FormControl(),
       start: new FormControl(10),
       end: new FormControl(500000),
+      all: new FormControl(false),
+      uploadList: new FormGroup({
+        ids: new FormControl(),
+        browse: new FormControl(),
+      })
     });
+  }
+
+  onFileChange(event) {
+    let reader = new FileReader();
+    let ids = this.annotationForm.controls.uploadList['controls'].ids;
+
+    //console.log(event, control)
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsText(file);
+
+      reader.onload = () => {
+        //    this.geneForm.patchValue({
+        //   file: reader.result
+        //   });
+        // console.log(reader.result)
+        ids.setValue(reader.result);
+        // this.cd.markForCheck();
+      };
+    }
   }
 
   submit() {
